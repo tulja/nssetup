d029750a29087139f515e7732e2c5ea1686d2d5f
tulja <btuljavamshikiran@gmail.com>
2018-01-29 08:08:51 +0530

    commented last few lines

--- replace_mmecpp_ip.sh
+++ replace_mmecpp_ip.sh
@@ -5,8 +5,8 @@ echo "Changing mme.cpp"
 
 sudo sed -i "s/^\(string\ g_trafmon_ip_addr\s*=\s*\).*\$/\1"$g_ran_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/mme.cpp
 sudo sed -i "s/^\(string\ g_mme_ip_addr\s*=\s*\).*\$/\1"$g_mme_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/mme.cpp
-sudo sed -i "s/^\(string\ g_hss_ip_addr\s*=\s*\).*\$/\1"$g_hss_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/mme.cpp
-sudo sed -i "s/^\(string\ g_sgw_s11_ip_addr\s*=\s*\).*\$/\1"$g_sgw_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/mme.cpp
-sudo sed -i "s/^\(string\ g_sgw_s1_ip_addr\s*=\s*\).*\$/\1"$g_sgw_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/mme.cpp
-sudo sed -i "s/^\(string\ g_sgw_s5_ip_addr\s*=\s*\).*\$/\1"$g_sgw_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/mme.cpp
-sudo sed -i "s/^\(string\ g_pgw_s5_ip_addr\s*=\s*\).*\$/\1"$g_pgw_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/mme.cpp
+#sudo sed -i "s/^\(string\ g_hss_ip_addr\s*=\s*\).*\$/\1"$g_hss_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/mme.cpp
+#sudo sed -i "s/^\(string\ g_sgw_s11_ip_addr\s*=\s*\).*\$/\1"$g_sgw_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/mme.cpp
+#sudo sed -i "s/^\(string\ g_sgw_s1_ip_addr\s*=\s*\).*\$/\1"$g_sgw_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/mme.cpp
+#sudo sed -i "s/^\(string\ g_sgw_s5_ip_addr\s*=\s*\).*\$/\1"$g_sgw_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/mme.cpp
+#sudo sed -i "s/^\(string\ g_pgw_s5_ip_addr\s*=\s*\).*\$/\1"$g_pgw_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/mme.cpp
