57ca3e66ba7c802fd3ca88d54ad3f1f425dc2cee
tulja <btuljavamshikiran@gmail.com>
2018-01-29 08:32:42 +0530

    Update replace_rancpp_ip.sh

--- replace_rancpp_ip.sh
+++ replace_rancpp_ip.sh
@@ -3,6 +3,6 @@ cd /opt/openbaton/scripts
 source "ip_map.sh" 
 echo "Changing ran.cpp"
 
-sudo sed -i "s/^\(string\ g_ran_ip_addr\s*=\s*\).*\$/\1"$g_ran_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/ran.cpp
-sudo sed -i "s/^\(string\ g_trafmon_ip_addr\s*=\s*\).*\$/\1"$g_ran_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/ran.cpp
-sudo sed -i "s/^\(string\ g_mme_ip_addr\s*=\s*\).*\$/\1"$g_mme_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/ran.cpp
+sudo sed -i "s/^\(string\ g_ran_ip_addr\s*=\s*\).*\$/\1"$g_ran_ip_addr"/" /opt/openbaton/scripts/LTECode/ran.cpp
+sudo sed -i "s/^\(string\ g_trafmon_ip_addr\s*=\s*\).*\$/\1"$g_ran_ip_addr"/" /opt/openbaton/scripts/LTECode/ran.cpp
+sudo sed -i "s/^\(string\ g_mme_ip_addr\s*=\s*\).*\$/\1"$g_mme_ip_addr"/" /opt/openbaton/scripts/LTECode/ran.cpp
