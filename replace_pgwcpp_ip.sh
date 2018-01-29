fd633481c04b78ac2a1ececeec9a1be5009061ad
tulja <btuljavamshikiran@gmail.com>
2018-01-29 08:33:52 +0530

    Update replace_pgwcpp_ip.sh

--- replace_pgwcpp_ip.sh
+++ replace_pgwcpp_ip.sh
@@ -3,7 +3,7 @@ cd /opt/openbaton/scripts
 source "ip_map.sh" 
 echo "Changing pgw.cpp"
 
-sudo sed -i "s/^\(string\ g_sgw_s5_ip_addr\s*=\s*\).*\$/\1"$g_sgw_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/pgw.cpp
-sudo sed -i "s/^\(string\ g_pgw_s5_ip_addr\s*=\s*\).*\$/\1"$g_pgw_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/pgw.cpp
-sudo sed -i "s/^\(string\ g_pgw_sgi_ip_addr\s*=\s*\).*\$/\1"$g_pgw_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/pgw.cpp
-sudo sed -i "s/^\(string\ g_sink_ip_addr\s*=\s*\).*\$/\1"$g_sink_ip_addr"/" /opt/openbaton/scripts/NFV_LTE_EPC-1.0/src/pgw.cpp
+sudo sed -i "s/^\(string\ g_sgw_s5_ip_addr\s*=\s*\).*\$/\1"$g_sgw_ip_addr"/" /opt/openbaton/scripts/LTECode/pgw.cpp
+sudo sed -i "s/^\(string\ g_pgw_s5_ip_addr\s*=\s*\).*\$/\1"$g_pgw_ip_addr"/" /opt/openbaton/scripts/LTECode/pgw.cpp
+sudo sed -i "s/^\(string\ g_pgw_sgi_ip_addr\s*=\s*\).*\$/\1"$g_pgw_ip_addr"/" /opt/openbaton/scripts/LTECode/pgw.cpp
+sudo sed -i "s/^\(string\ g_sink_ip_addr\s*=\s*\).*\$/\1"$g_sink_ip_addr"/" /opt/openbaton/scripts/LTECode/pgw.cpp
