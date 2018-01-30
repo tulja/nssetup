01e5f46814a0f36024b31ab3589b60661f2697ed
tulja <btuljavamshikiran@gmail.com>
2018-01-30 15:36:14 +0530

    Update server_configure_mme.sh

--- server_configure_mme.sh
+++ server_configure_mme.sh
@@ -1,1 +1,13 @@
+echo "Change the Directory to /opt/openbaton/scripts"
+cd /opt/openbaton/scripts/
+echo "Running  the Server Configure at Client" 
+echo "Server IP is " 
+echo $server_private
+echo "Server Hostname is "
+echo $server_hostname 
 
+
+#if server hostname == sgw1 write server_private to sgw1.txt 
+
+
+#if server hostname == pgw1 Write server_private to sgw1.txt 
